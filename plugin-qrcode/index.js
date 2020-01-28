52a7aa7e1ddea7a84ae49b61ccee0ac91cb0afdb
Shigma <1700011071@pku.edu.cn>
2020-01-28 15:55:21 +0800

    remove deprecated syntax

--- plugin-qrcode/index.js
+++ plugin-qrcode/index.js
@@ -4,9 +4,9 @@ module.exports.name = 'qrcode'
 
 module.exports.apply = (ctx) => {
   ctx.command('qrcode <text>', '生成二维码')
-    .option('-m, --margin <margin>', '边界尺寸', { type: Number, default: 4 })
-    .option('-s, --scale <scale>', '比例系数', { type: Number, default: 4 })
-    .option('-w, --width <width>', '图片大小', { type: Number, default: undefined })
+    .option('-m, --margin <margin>', '边界尺寸', { default: 4 })
+    .option('-s, --scale <scale>', '比例系数', { default: 4 })
+    .option('-w, --width <width>', '图片大小', { default: undefined })
     .option('-d, --dark <color>', '暗部颜色')
     .option('-l, --light <color>', '亮部颜色')
     .action(async ({ meta, options }, text) => {
