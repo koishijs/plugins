1199644882b5c8a18d3ab777bca953c7cff62b81
NN708 <nn708@outlook.com>
2020-01-28 15:52:54 +0800

    Update plugin-weather

--- plugin-weather/index.js
+++ plugin-weather/index.js
@@ -2,15 +2,17 @@ const axios = require('axios')
 
 const lang = 'zh-CN'
 const unit = 'metric'
+const products = ['astro', 'civil', 'civillight', 'meteo', 'two']
 
 module.exports.name = 'weather'
 
 module.exports.apply = (ctx) => {
   ctx.command('weather <longitude> <latitude>', '查询天气')
-    .option('-p, --product <product>', '晴天钟产品选择，可为 astro, civil, civillight, meteo 或 two', { type: String, default: 'civil' })
+    .option('-p, --product <product>', `晴天钟产品选择，可为 ${products.join(', ')}`, { default: 'civil' })
     .action(async ({ meta, options }, lon, lat) => {
       if (!lon || !lat) return meta.$send('请输入经纬度')
       const { product } = options
+      if (!products.includes(options.product)) return meta.$send(`不支持该产品，产品选择应为 ${products.join(', ')} 之一`)
       try {
         const { data } = await axios.get(`http://www.7timer.info/bin/${product}.php`, {
           params: { lon, lat, lang, unit },
